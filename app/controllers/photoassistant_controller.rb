5bebd4ab0bdbf8272df9303f4284a226179cb9c2
amccadams <amccadams@gmail.com>
2013-05-24 09:17:22 -0500

    adds equipment index

--- app/controllers/photoassistant_controller.rb
+++ app/controllers/photoassistant_controller.rb
@@ -2,17 +2,36 @@ class PhotoAssistantController
   # include Formatter
   
   def initialize params
-    @params = params  
+    # @params = params  
+  end
+
+def index
+    equipments = Equipment.all
+    equipments.each_with_index do |equipment, i|
+      puts "#{i+1}. #{equipment.name}"
+    end
   end
 
   def create
-    equipment = Equipment.new(params[:equipment])
-    if equipment.save 
-      puts 'Success!'
+    equipments = Equipment.new(name: equipments)
+    if equipments.save
+      puts "Success!"
     else
-      puts 'Failure: #{equipment.errors.full_messages.join (",")}'
+      puts "Failure :( #{equipments.errors.full_messages.join(", ")}"
     end
   end
+
+
+
+
+  # def create
+  #   equipment = Equipment.new(params[:equipment])
+  #   if equipment.save 
+  #     puts 'Success!'
+  #   else
+  #     puts 'Failure: #{equipment.errors.full_messages.join (",")}'
+  #   end
+  # end
  
  
 end
